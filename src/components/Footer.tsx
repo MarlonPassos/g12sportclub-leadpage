b6ff4236600a2ed8bbe064258e330b5cd969bdce
Marlon Passos <marlonjbpassos@gmail.com>
2024-11-10 21:24:20 -0300

    feat: adiciona componente Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import FacebookIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/X';
+import { colors } from '@mui/material';
+// import SitemarkIcon from './SitemarkIcon';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" sx={{ color: 'text.secondary', mt: 1 }}>
+      {'Copyright © '}
+      <Link color="text.secondary" href="/">
+        Zior
+      </Link>
+      &nbsp;
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
+export default function Footer() {
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: { xs: 4, sm: 8 },
+        py: { xs: 1, sm: 10 },
+        textAlign: { sm: 'center', md: 'left' },
+        bgcolor: colors.grey[100]
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column', sm: 'row' },
+          width: '100%',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 4,
+            minWidth: { xs: '100%', sm: '60%' },
+          }}
+        >
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          pt: { xs: 4, sm: 8 },
+          width: '100%',
+          borderTop: '1px solid',
+          borderColor: 'divider',
+        }}
+      >
+        <div>
+          <Link color="text.secondary" variant="body2" href="#">
+            Política de Privacidade
+          </Link>
+          <Typography sx={{ display: 'inline', mx: 0.5, opacity: 0.5 }}>
+            &nbsp;•&nbsp;
+          </Typography>
+          <Link color="text.secondary" variant="body2" href="#">
+            Termos de Serviço
+          </Link>
+          <Copyright />
+        </div>
+        <Stack
+          direction="row"
+          spacing={1}
+          useFlexGap
+          sx={{ justifyContent: 'left', color: 'text.secondary' }}
+        >
+          <IconButton
+            color="inherit"
+            size="small"
+            href="/"
+            aria-label="GitHub"
+            sx={{ alignSelf: 'center' }}
+          >
+            <FacebookIcon />
+          </IconButton>
+          <IconButton
+            color="inherit"
+            size="small"
+            href="/"
+            aria-label="X"
+            sx={{ alignSelf: 'center' }}
+          >
+            <TwitterIcon />
+          </IconButton>
+          <IconButton
+            color="inherit"
+            size="small"
+            href="/"
+            aria-label="LinkedIn"
+            sx={{ alignSelf: 'center' }}
+          >
+            <LinkedInIcon />
+          </IconButton>
+        </Stack>
+      </Box>
+    </Container>
+  );
+}
--- src/components/OndeEstamos.tsx
+++ src/components/OndeEstamos.tsx
@@ -2,88 +2,87 @@ import React from 'react';
 import Title from './Title';
 import { Box, Stack, Typography } from '@mui/material';
 
-export default function OndeEstamos() {
+const OndeEstamos = () => {
   return (
     <Box id="onde-estamos" sx={{ paddingTop: '80px' }}>
       <Title text="Onde Estamos" />
-      <Stack
-        direction={{ xs: 'column', md: 'row' }}
-        mt={2}
-        spacing={4}
-        alignContent="center"
-        alignItems="center"
-      >
-        <Box
-          width={{ xs: '100%', md: '42%' }}
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          <Box
-            component="img"
-            src={"/images/mapa.png"}
-            alt="Imagem da História G12"
-            sx={{
-              width: '100%',
-              maxHeight: {
-                xs: '200px', sm: '300px'
-              },
-              height: 'auto',
-              objectFit: 'cover',
-              display: 'block',
-            }}
-          />
-        </Box>
+      <Stack sx={{ gap: 5 }}>
         <Stack
-          direction="column"
-          width={{ xs: '100%', md: '58%' }}
-          sx={{ gap: 2 }}
+          direction={{ xs: 'column', md: 'row' }}
+          mt={2}
+          spacing={4}
+          alignContent="center"
+          alignItems="center"
         >
-          <Typography variant="subtitle1" fontSize={{ xs: 16, sm: 18, md: 20 }}>
-            O G12 Sport Club está localizado no coração da cidade, em um ambiente acessível e rodeado pela natureza, o que proporciona um espaço ideal para o desenvolvimento esportivo e pessoal de nossos alunos. Estamos situados na Avenida das Palmeiras, 2500, em um bairro tranquilo e seguro, com fácil acesso a transporte público e várias comodidades, como praças e centros comerciais.
-          </Typography>
-        </Stack>
-      </Stack>
-      <Stack
-        direction={{ xs: 'column', md: 'row' }}
-        mt={2}
-        spacing={4}
-        alignContent="center"
-        alignItems="center"
-      >
-        <Stack
-          direction="column"
-          width={{ xs: '100%', md: '58%' }}
-          sx={{ gap: 2 }}
-        >
-          <Typography variant="subtitle1" fontSize={{ xs: 16, sm: 18, md: 20 }}>
-            O G12 Sport Club está localizado no coração da cidade, em um ambiente acessível e rodeado pela natureza, o que proporciona um espaço ideal para o desenvolvimento esportivo e pessoal de nossos alunos. Estamos situados na Avenida das Palmeiras, 2500, em um bairro tranquilo e seguro, com fácil acesso a transporte público e várias comodidades, como praças e centros comerciais.
-          </Typography>
-        </Stack>
-        <Box
-          width={{ xs: '100%', md: '42%' }}
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
+          <Stack
+            direction="column"
+            width={{ xs: '100%', md: '58%' }}
+            sx={{ gap: 2 }}
+          >
+            <Typography variant='h6'>Quadra Coberta</Typography>
+            <Typography variant="subtitle1" fontSize={{ xs: 16, sm: 18, md: 20 }}>
+              A Quadra Coberta está localizada no coração de Paranoá, em Brasília, em frente à Paróquia Santa Maria dos Pobres. Esse ponto de fácil acesso oferece um ambiente seguro e confortável para a prática de esportes e atividades comunitárias, com a vantagem de estar próximo de um marco importante da região, a Paróquia Santa Maria dos Pobres.
+            </Typography>
+          </Stack>
           <Box
-            component="img"
-            src={"/images/mapa.png"}
-            alt="Imagem da História G12"
+            width={{ xs: '100%', md: '42%' }}
             sx={{
-              width: '100%',
-              maxHeight: {
-                xs: '200px', sm: '300px'
-              },
-              height: 'auto',
-              objectFit: 'cover',
-              display: 'block',
+              display: 'flex',
+              justifyContent: 'center',
             }}
-          />
-        </Box>
+          >
+            {/* Mapa incorporado com iframe */}
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d957.3744639489796!2d-47.78034176177644!3d-15.777750665466051!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a3d4114396135%3A0xd795ad151014f056!2sQuadra%20Coberta!5e1!3m2!1sen!2sbr!4v1731283327449!5m2!1sen!2sbr"
+              width="100%"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </Box>
+        </Stack>
+
+        <Stack
+          direction={{ xs: 'column', md: 'row' }}
+          mt={2}
+          spacing={4}
+          alignContent="center"
+          alignItems="center"
+        >
+          <Stack
+            direction="column"
+            width={{ xs: '100%', md: '58%' }}
+            sx={{ gap: 2 }}
+          >
+            <Typography variant='h6'>Campo Sintético de Paranoá</Typography>
+            <Typography variant="subtitle1" fontSize={{ xs: 16, sm: 18, md: 20 }}>
+              O Campo Sintético de Paranoá está estrategicamente localizado, próximo ao Balão do Itapuã, um dos pontos mais conhecidos da região. Este espaço oferece uma excelente infraestrutura para a prática de esportes, sendo de fácil acesso para quem vem de diferentes partes da cidade. Se você está buscando um local ideal para suas atividades esportivas, venha conhecer o Campo Sintético de Paranoá, com comodidade e qualidade.
+            </Typography>
+          </Stack>
+          <Box
+            width={{ xs: '100%', md: '42%' }}
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            {/* Mapa incorporado com iframe */}
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d957.4624754054047!2d-47.77705589183698!3d-15.75909839429519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a3d630789c5fd%3A0x27db7b013d6fa854!2sCampo%20Sint%C3%A9tico%20de%20Parano%C3%A1!5e1!3m2!1sen!2sbr!4v1731283429461!5m2!1sen!2sbr"
+              width="100%"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </Box>
+        </Stack>
       </Stack>
     </Box>
   );
-}
+};
+
+export default OndeEstamos;
